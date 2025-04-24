90e54e8f49f1875bf51d99e5b7dd8f9a5e1ff602
tilly <till.aust@uni-konstanz.de>
2025-04-24 17:05:01 +0200

    updated constants

--- constants.py
+++ constants.py
@@ -1,15 +1,18 @@
-tills_mbp = True
+import uuid
 
-if tills_mbp:
-    OUTPUT_DIR = "agg_data"  # Path to base folder for the output files
-    BASE_DIR = "/Users/mariya_ty/Desktop/BA projekt"  # Path to the base folder for the input files
-    FILTERED_DIR = "filtered_data"  # Path to the base folder for the filtered files
+def get_machine_id():
+    return hex(uuid.getnode())
+
+if get_machine_id() == '0xf2925d19892a':  # tills laptop
+    OUTPUT_DIR = "/Volumes/Data/chronopilot/external_datasets/agg_data"  # Path to base folder for the output files
+    BASE_DIR = "/Volumes/Data/chronopilot/external_datasets"  # Path to the base folder for the input files
+    FILTERED_DIR = "/Volumes/Data/chronopilot/external_datasets/filtered_data"  # Path to the base folder for the filtered files
     # Window settings in samples (72 sec window, 20 sec step)
     INTERVAL = 72
     STEP = 20
 else:
     OUTPUT_DIR = "agg_data"  # TODO check if this is correct
-    BASE_DIR = "BA projekt"  # TODO check if this is correct
+    BASE_DIR = "/Users/mariya_ty/Desktop/BA projekt"  # Path to the base folder for the input files
     FILTERED_DIR = "filtered_data"  # TODO check if this is correct
     INTERVAL = 72
     STEP = 20
